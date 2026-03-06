76c6a93a037585211ee50b3650fd8247082201b2
Andres <andresemiliomujica2006@gmail.com>
2026-03-06 14:17:14 -0400

    Feat: dependencias de colletion y url_launcher agregadas, Fix: validators, papal_sevice y paypal_screen Arreglados

---
+++ lib/screens/payments/paypal_sdk_payment_screen.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+import '../../services/paypal_sdk_service.dart';
+import '../../widgets/primary_button.dart';
+
+class PayPalSDKPaymentScreen extends StatefulWidget {
+  final double amount;
+
+  const PayPalSDKPaymentScreen({super.key, required this.amount});
+
+  @override
+  State<PayPalSDKPaymentScreen> createState() => _PayPalSDKPaymentScreenState();
+}
+
+class _PayPalSDKPaymentScreenState extends State<PayPalSDKPaymentScreen> {
+  final paypal = PayPalSDKService();
+  bool loading = false;
+
+  Future<void> pay() async {
+    setState(() => loading = true);
+
+    final url = await paypal.createOrder(widget.amount);
+
+    setState(() => loading = false);
+
+    if (url != null) {
+      await launchUrl(Uri.parse(url), mode: LaunchMode.externalApplication);
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text("Error creando orden PayPal")),
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Pagar con PayPal (SDK)")),
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          children: [
+            Text("Monto a pagar: \$${widget.amount}"),
+            const SizedBox(height: 20),
+
+            PrimaryButton(
+              text: "Pagar con PayPal",
+              loading: loading,
+              onPressed: pay,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
