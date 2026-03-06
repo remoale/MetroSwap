0425d78ba90c6aeda654798e5a5b9b2e80346f32
diegoguz-hub <diegoguz@correo.unimet.edu.ve>
2026-03-05 21:57:19 -0400

    estructura del layout

--- lib/screens/publish/publish_screen.dart
+++ lib/screens/publish/publish_screen.dart
@@ -2,8 +2,18 @@ import 'package:flutter/material.dart';
 import 'package:metroswap/widgets/metroswap_footer.dart';
 import 'package:metroswap/widgets/metroswap_navbar.dart';
 
-class PublishScreen extends StatelessWidget {
+class PublishScreen extends StatefulWidget {
   const PublishScreen({super.key});
+
+  @override
+
+  State<PublishScreen> createState() => _PublishScreenState();
+}
+
+class _PublishScreenState extends State<PublishScreen>{
+  //Controlar dropdown
+  String? _selectedCategory;
+  String? _selectedMethod;
 
   @override
   Widget build(BuildContext context) {
@@ -12,18 +22,37 @@ class PublishScreen extends StatelessWidget {
       body: Column(
         children: [
           const MetroSwapNavbar(developmentNav: true, heading: 'Publicar'),
-          const Expanded(
-            child: Center(
-              child: Text(
-                'En desarrollo',
-                style: TextStyle(fontSize: 20),
+          Expanded(
+            child:SingleChildScrollView(
+              padding : const EdgeInsets.all(40.0),
+              child: Center(
+              child: Container(
+                constraints: const BoxConstraints(maxWidth: 1000),
+
+                child: Row(
+                  crossAxisAlignment:CrossAxisAlignment.start,
+                  children: [
+                    //lado izq
+                    Expanded(
+                      flex:1,
+                      child: _buildImagePlaceholder(),),
+                      const SizedBox(width: 50),
+
+                      // lado derecho
+                      Expanded(
+                        flex: 1,
+                        child: _buildFormFields(),
+                      ),
+                  ],
+                  ),
               ),
             ),
+          ),
           ),
           const MetroSwapFooter(),
         ],
       ),
     );
   }
-}
 
+}
